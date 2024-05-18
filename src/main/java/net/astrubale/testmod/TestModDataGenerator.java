fd59699601eb78965b287cb53d6d63e9b5e875f4
Astrubale <nic04.simionato@gmail.com>
2024-05-18 23:49:10 +0200

    default fabric file commit 2

--- src/main/java/net/astrubale/testmod/TestModDataGenerator.java
+++ src/main/java/net/astrubale/testmod/TestModDataGenerator.java
@@ -1,4 +1,4 @@
-package net.astrubale;
+package net.astrubale.testmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
--- src/main/java/net/astrubale/testmod/mixin/ExampleMixin.java
+++ src/main/java/net/astrubale/testmod/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package net.astrubale.mixin;
+package net.astrubale.testmod.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
