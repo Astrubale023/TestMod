aa5425852206753e77d4aeaa0fdf09a7929ec38d
Astrubale <nic04.simionato@gmail.com>
2024-05-19 09:46:27 +0200

    modBlocks and its register

---
+++ src/main/java/net/astrubale/testmod/block/ModBlocks.java
@@ -0,0 +1,45 @@
+package net.astrubale.testmod.block;
+
+import net.astrubale.testmod.TestMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.PillarBlock;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+public class ModBlocks {
+
+    public static final Block CHESTNUT_LOG = registerBlock("chestnut_log",
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_LOG).strength(4f)));
+    public static final Block CHESTNUT_WOOD = registerBlock("chestnut_wood",
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_WOOD).strength(4f)));
+    public static final Block STRIPPED_CHESTNUT_LOG = registerBlock("stripped_chestnut_log",
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.STRIPPED_OAK_LOG).strength(4f)));
+    public static final Block STRIPPED_CHESTNUT_WOOD = registerBlock("stripped_chestnut_wood",
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.STRIPPED_OAK_WOOD).strength(4f)));
+
+    public static final Block CHESTNUT_PLANKS = registerBlock("chestnut_planks",
+            new Block(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS).strength(4f)));
+    public static final Block CHESTNUT_LEAVES = registerBlock("chestnut_leaves",
+            new LeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).strength(4f).nonOpaque()));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(TestMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(TestMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerModBlocks() {
+        TestMod.LOGGER.info("Registering ModBlocks for " + TestMod.MOD_ID);
+    }
+}
