b825316935aef2054105fd8dbdb493fd27bbd3e6
Astrubale <nic04.simionato@gmail.com>
2024-05-19 09:30:15 +0200

    wordl datagen setup

--- src/main/java/net/astrubale/testmod/TestModDataGenerator.java
+++ src/main/java/net/astrubale/testmod/TestModDataGenerator.java
@@ -1,6 +1,7 @@
 package net.astrubale.testmod;
 
 import net.astrubale.testmod.world.biome.ModBiomes;
+import net.astrubale.testmod.datagen.ModWorldGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.registry.RegistryBuilder;
@@ -9,7 +10,9 @@ import net.minecraft.registry.RegistryKeys;
 public class TestModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModWorldGenerator::new);
 	}
 
 	@Override
