def4f8b293c609a1dc1ee06270501cade54b642b
Astrubale <nic04.simionato@gmail.com>
2024-05-19 11:18:48 +0200

    unknown commit

--- src/main/java/net/astrubale/testmod/datagen/ModModelProvider.java
+++ src/main/java/net/astrubale/testmod/datagen/ModModelProvider.java
@@ -5,12 +5,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Model;
-import net.minecraft.data.client.Models;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.util.Identifier;
-
-import java.util.Optional;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
