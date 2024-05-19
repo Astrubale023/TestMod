b193231f18e96a103bf395e3238dea14ecf74ac5
Astrubale <nic04.simionato@gmail.com>
2024-05-19 09:18:35 +0200

    Terrablender API done

--- src/main/java/net/astrubale/testmod/world/biome/ModTerrablenderAPI.java
+++ src/main/java/net/astrubale/testmod/world/biome/ModTerrablenderAPI.java
@@ -1,11 +1,15 @@
 package net.astrubale.testmod.world.biome;
 
+import net.astrubale.testmod.TestMod;
+import net.minecraft.util.Identifier;
+import terrablender.api.Region;
+import terrablender.api.Regions;
 import terrablender.api.TerraBlenderApi;
 
 public class ModTerrablenderAPI implements TerraBlenderApi {
 
     @Override
     public void onTerraBlenderInitialized() {
-        TerraBlenderApi.super.onTerraBlenderInitialized();
+        Regions.register(new ModOverworldRegion(new Identifier(TestMod.MOD_ID, "overworld"), 4));
     }
 }
