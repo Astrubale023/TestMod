c14e81cbb6793a9f1977975efbb509962bd29584
Astrubale <nic04.simionato@gmail.com>
2024-05-19 09:14:09 +0200

    DataGen registry Biome

--- src/main/java/net/astrubale/testmod/TestModDataGenerator.java
+++ src/main/java/net/astrubale/testmod/TestModDataGenerator.java
@@ -1,11 +1,19 @@
 package net.astrubale.testmod;
 
+import net.astrubale.testmod.world.biome.ModBiomes;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class TestModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.BIOME, ModBiomes::boostrap);
 	}
 }
